fcce156f22a7dba9846ff479360cc781678e0964
evandrocoan <evandrocoan@hotmail.com>
2017-07-21 11:41:20 -0300

    Created the codedev.bat to run it with the console output.

---
+++ codedev.bat
@@ -0,0 +1,23 @@
+@echo off
+
+:: Command Line
+:: https://code.visualstudio.com/docs/editor/command-line
+
+
+:: Specifies the directory that user data is kept in, useful when running as root.
+:: --user-data-dir <dir>
+
+:: Set the root path for extensions.
+:: --extensions-dir <dir>
+
+:: All command line arguments passed to this file
+:: %*
+
+:: Windows - Run process on background after closing cmd
+:: https://superuser.com/questions/1069972/windows-run-process-on-background-after-closing-cmd
+start /B /min .\source\scripts\code.bat --user-data-dir "%~dp0settings" --extensions-dir "%~dp0settings\extensions" %*
+
+
+
+
+
